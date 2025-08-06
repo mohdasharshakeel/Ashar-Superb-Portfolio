aab3dbe03dca9b926e3df9ab4374e29648f3a970
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-08-06 13:14:44 +0530

    create tailwind custom plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
-module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+import animatePlugin from 'tailwindcss-animate';
+import { tailwindPlugin } from './app/_lib';
+
+/** @type {import('tailwindcss').Config} */
+const tailwindConfig = {
+  content: ['./components/**/*.{js,jsx,mdx}', './app/**/*.{js,jsx,mdx}'],
+  plugins: [tailwindPlugin, require('tailwindcss-animate')],
+};
+
+module.exports = tailwindConfig;
